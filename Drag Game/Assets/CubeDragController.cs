9e9f9747e9ae7c41ba8ffbe4338f0222e6ac2d2b
SalimShebli <salim.shebli1996@gmail.com>
2019-03-13 19:34:00 +0200

    init project

---
+++ Drag Game/Assets/CubeDragController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeDragController : MonoBehaviour {
+
+	// Use this for initialization
+    Vector3 lastPostion;
+    bool overlap = false;
+
+	void Start () {
+        lastPostion = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private Vector3 screenPoint;
+ private Vector3 offset;
+ 
+ void OnMouseDown()
+ {
+     screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+     offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+ }
+ 
+    void OnMouseDrag()
+    {
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+            curPosition.y = transform.position.y;
+            transform.position = curPosition;
+            
+            
+    }
+    void OnMouseUp()
+    {
+      
+        if(overlap)
+        {
+            transform.position = lastPostion;
+            overlap = false;
+        }
+    }
+    void OnTriggerEnter()
+    {
+        print("trrr");
+        overlap = true;
+    }
+    void OnTriggerExit()
+    {
+        print("enbnnn");
+        overlap = false;
+
+    }
+   
+}
